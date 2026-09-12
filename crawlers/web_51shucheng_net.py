caba6c4131b264d6c7edcbe9fb66654cf77ee8f0
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2026-09-13 00:43:01 +1200

    Update web_51shucheng_net.py

--- crawlers/web_51shucheng_net.py
+++ crawlers/web_51shucheng_net.py
@@ -1,58 +1,51 @@
 import json
 import logging
-import os
 import re
 
+from DrissionPage import ChromiumPage
 from bs4 import BeautifulSoup
-from requests import Session
 
-chrome_135 = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
-              'application/signed-exchange;v=b3;q=0.7',
-    'accept-encoding': 'gzip, deflate',
-    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-    'cache-control': 'max-age=0',
-    'dnt': '1',
-    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"', 'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"', 'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none', 'sec-fetch-user': '?1', 'upgrade-insecure-requests': '1',
-    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
-}
-session = Session()
-if os.path.isfile('proxies.json'):
-    with open('proxies.json', 'r') as f:
-        proxies = json.load(f)
-    session.proxies = proxies
-else:
-    session.trust_env = False
+from cloudflare_bypasser import CloudflareBypasser
 
+chrome = ChromiumPage()
 
 def get_meta(source):
-    cover_page = session.get(url=source, headers=chrome_135)
-    if cover_page.status_code != 200:
-        raise Exception(f'Fail to get the cover page. Status code: {cover_page.status_code}.')
-    else:
-        logging.info('Success to get the cover page.')
-    cover_page.encoding = 'utf-8'
-    cover_text = BeautifulSoup(cover_page.text, features='html.parser')
-    chapter_list = cover_text.find('div', {'class': 'mulu-list'}).find_all('li')
+    chrome.get(source)
+    cf_bypasser = CloudflareBypasser(chrome)
+    cf_bypasser.bypass()
+
+    cover_page = chrome.html
+    cover_text = BeautifulSoup(cover_page, features='html.parser')
+    logging.info('Success to get the cover page.')
+
+    chapter_list = cover_text.find('ul', {'id': 'toc-list'}).find_all('li')
     chapter_list = [[x.a.text, x.a['href']] for x in chapter_list]
-    book = {
-        'title': cover_text.find('div', {'class': 'catalog'}).h1.text,
-        'author': re.sub('.*作者：', '', cover_text.find('div', {'class': 'info'}).text.strip()),
-        'abstract': cover_text.find('div', {'class': 'intro'}).text,
-        'chapter_list': chapter_list,
-    }
+
+    for js_script in cover_text.find_all('script', {'type': 'application/ld+json'}):
+        try:
+            js_dict = json.loads(js_script.text)
+        except json.JSONDecodeError:
+            continue
+        if js_dict.get("@type") == "Book":
+            book = {
+                "abstract": js_dict["description"],
+                "title": js_dict["name"],
+                "author": js_dict["author"]["name"],
+                'chapter_list': chapter_list,
+            }
+            break
+    else:
+        raise Exception("Cannot find book meta data in cover page.")
     return book
 
 
 def get_chapter(source):
-    chapter_page = session.get(url=source, headers=chrome_135)
-    if chapter_page.status_code != 200:
-        logging.warning(f'Fail to download {source}. Status code: {chapter_page.status_code}. ')
-        return
-    chapter_page.encoding = 'utf-8'
-    chapter_text = BeautifulSoup(chapter_page.text, features='html.parser')
+    chrome.get(source)
+    cf_bypasser = CloudflareBypasser(chrome)
+    cf_bypasser.bypass()
+    chapter_page = chrome.html
+    chapter_text = BeautifulSoup(chapter_page, features='html.parser')
+    logging.info(f"Successfully getting chapter {source}")
     chapter_title = chapter_text.find('h1').text
     chapter_normal = chapter_text.find('div', {'class': 'neirong'}).text
     chapter_normal = chapter_normal.replace('\xa0', '')
